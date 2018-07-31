85d2c2694165887ad1602c85068f7216b87398aa
Danny Guo <dannyguo91@gmail.com>
2018-07-31 14:53:45 -0400

    Add Termux support

---
+++ applications/termux/README.md
@@ -0,0 +1,9 @@
+# Blood Moon for Termux
+
+Installation:
+
+```bash
+$ pkg install curl
+$ curl -Lo ~/.termux/colors.properties --create-dirs https://raw.githubusercontent.com/dguo/blood-moon/master/applications/termux/blood-moon.properties
+$ termux-reload-settings
+```
---
+++ applications/termux/template.ts
@@ -0,0 +1,36 @@
+import {ColorScheme, TemplateOutput} from '../../types';
+
+export function generate(name: string, colors: ColorScheme): TemplateOutput {
+  const ansi = [
+    colors.black,
+    colors.red,
+    colors.green,
+    colors.yellow,
+    colors.blue,
+    colors.magenta,
+    colors.cyan,
+    colors.white,
+    colors.gray,
+    colors.lightRed,
+    colors.lightGreen,
+    colors.lightYellow,
+    colors.lightBlue,
+    colors.pink,
+    colors.lightCyan,
+    colors.lightWhite
+  ];
+
+  const palette = ansi.map((color, index) => {
+    return `color${index}:${color}`;
+  }).join('\n');
+
+  const content = `# https://github.com/dguo/blood-moon
+background=${colors.background}
+foreground=${colors.foreground}
+${palette}`;
+
+  return {
+    fileName: `${name}.properties`,
+    content
+  };
+}
