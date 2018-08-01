59b584a51e932a38d6e15f3e06b4d16516fa7f8c
Danny Guo <dannyguo91@gmail.com>
2018-07-31 20:38:03 -0400

    Add Slack support

---
+++ applications/slack/README.md
@@ -0,0 +1,6 @@
+# Blood Moon for Slack
+
+## Installation
+
+1. Go to Preferences > Sidebar > Theme > Custom Theme.
+2. Paste the values in `blood-moon-slack.txt`.
---
+++ applications/slack/template.ts
@@ -0,0 +1,29 @@
+import {ColorScheme, TemplateOutput} from '../../types';
+
+export function generate(name:string, colors: ColorScheme): TemplateOutput {
+  /* Order is:
+     Column BG,
+     Menu BG Hover,
+     Active Item,
+     Active Item Text,
+     Hover Item,
+     Text Color,
+     Active Presence,
+     Mention Badge
+   */
+  const theme = [
+    colors.background,
+    colors.lightWhite,
+    colors.selection,
+    colors.selectionText,
+    colors.lightWhite,
+    colors.foreground,
+    colors.green,
+    colors.yellow
+  ];
+
+  return {
+    fileName: `${name}-slack.txt`,
+    content: theme.join(',')
+  };
+}
